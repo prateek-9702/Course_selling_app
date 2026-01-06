40864176dd7ea9a272201ab2b87a698b6febdffd
prateek <ansh-ansh@gmail.com>
2026-01-06 10:14:04 +0530

    Used dotenv for enviroment variable

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const JWT_USER_PASSWORD = "aseraser"
-const JWT_ADMIN_PASSWORD = "jhyuyyijhyy"
+const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD 
+const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD
 
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+console.log(process.env.MONGO_URL)
 const express = require("express");
 const mongoose = require("mongoose")
 const { userRouter } = require("./routes/user");
@@ -12,7 +14,7 @@ app.use("/api/v1/course", courseRouter);
 
 
 async function main(){
-  await mongoose.connect("mongodb+srv://prateek:b7GVcu7T29LcyM30@cluster0.cdjtxa7.mongodb.net/coursera-app")
+  await mongoose.connect("process.env.MONGO_URL")
   app.listen(3000);
   console.log("Listening on port 3000")
 }
